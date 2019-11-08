2326d89486dd95f1ea36afd2d9c43edf205706db
Martyn Pike <pike@adacore.com>
2019-11-08 21:50:49 +0000

    Reformatting with clauses and formal parameters

--- src/socket_layer-gnat.ads
+++ src/socket_layer-gnat.ads
@@ -1,4 +1,5 @@
-with Ada.Streams, GNAT.Sockets;
+with Ada.Streams;
+with GNAT.Sockets;
 
 package Socket_Layer is
 
@@ -18,12 +19,15 @@ package Socket_Layer is
    procedure Shutdown_Server (Server : Socket_Type);
 
    procedure Receive_Datagram
-     (Socket :     Socket_Type; Item : out Ada.Streams.Stream_Element_Array;
+     (Socket : Socket_Type;
+      Item   : out Ada.Streams.Stream_Element_Array;
       Last   : out Ada.Streams.Stream_Element_Offset;
       From   : out Socket_Address_Type);
 
    procedure Send_Datagram
-     (Socket :     Socket_Type; Item : Ada.Streams.Stream_Element_Array;
-      Last : out Ada.Streams.Stream_Element_Offset; To : Socket_Address_Type);
+     (Socket : Socket_Type;
+      Item   : Ada.Streams.Stream_Element_Array;
+      Last   : out Ada.Streams.Stream_Element_Offset;
+      To     : Socket_Address_Type);
 
 end Socket_Layer;

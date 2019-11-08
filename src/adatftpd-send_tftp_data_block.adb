99f6dc0b2082bcd0d70025bf674589f4ce141a0a
Martyn Pike <pike@adacore.com>
2019-11-08 14:13:59 +0000

    Applied pretty printer

--- src/adatftpd-send_tftp_data_block.adb
+++ src/adatftpd-send_tftp_data_block.adb
@@ -1,11 +1,10 @@
 separate (adatftpd)
-procedure Send_TFTP_Data_Block(
-   From_Server    : Socket_Layer.Socket_Type;
-   To_Client      : Socket_Layer.Socket_Address_Type;
-   Filename       : String;
-   Block_Number   : in out Interfaces.Unsigned_16;
-   Bytes_Sent     : in out Byte_IO.Count
-) is
+procedure Send_TFTP_Data_Block
+  (From_Server  :        Socket_Layer.Socket_Type;
+   To_Client    :        Socket_Layer.Socket_Address_Type; Filename : String;
+   Block_Number : in out Interfaces.Unsigned_16;
+   Bytes_Sent   : in out Byte_IO.Count)
+is
 
    use type Byte_IO.Count;
    use type Ada.Streams.Stream_Element_Array;
@@ -15,47 +14,37 @@ procedure Send_TFTP_Data_Block(
 
 begin
 
-   Byte_IO.Open(
-      File => File_Handle,
-      Mode => Byte_IO.In_File,
-      Name => Filename
-   );
+   Byte_IO.Open
+     (File => File_Handle, Mode => Byte_IO.In_File, Name => Filename);
 
    Block_Number :=
-      (if Block_Number = Interfaces.Unsigned_16'Last then 1 else Interfaces.Unsigned_16'Succ(Block_Number));
+     (if Block_Number = Interfaces.Unsigned_16'Last then 1
+      else Interfaces.Unsigned_16'Succ (Block_Number));
 
    declare
 
       use type Byte_IO.Positive_Count;
 
       Data_Part : constant Ada.Streams.Stream_Element_Array :=
-         Read_TFTP_File_Block(
-            From_File => File_Handle,
-            At_Index  => (Bytes_Sent + 1)
-         );
+        Read_TFTP_File_Block
+          (From_File => File_Handle, At_Index => (Bytes_Sent + 1));
 
       Datagram : constant Ada.Streams.Stream_Element_Array :=
-         From_U16_To_Bytes(TFTP_DATA)
-         &
-         From_U16_To_Bytes(From => Block_Number)
-         &
-         Data_Part;
+        From_U16_To_Bytes (TFTP_DATA) &
+        From_U16_To_Bytes (From => Block_Number) & Data_Part;
 
       Last_Sent : Ada.Streams.Stream_Element_Offset;
 
    begin
 
-      Socket_Layer.Send_Datagram(
-         Socket => From_Server,
-         Item   => Datagram,
-         Last   => Last_Sent,
-         To     => To_Client
-      );
+      Socket_Layer.Send_Datagram
+        (Socket => From_Server, Item => Datagram, Last => Last_Sent,
+         To     => To_Client);
 
       Bytes_Sent := Bytes_Sent + Data_Part'Length;
 
    end;
 
-   Byte_IO.Close(File => File_Handle);
+   Byte_IO.Close (File => File_Handle);
 
 end Send_TFTP_Data_Block;
